29af50444d5d8432626ef7a7c144d9f00439788a
codepanther <pezechigozirim@gmail.com>
2022-02-24 23:07:18 +0100

    Create the slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import './slider.css';
+import MySlider from 'react-perfect-slider';
+import spongeCake from '../../assets/themed_sponge_cake.jpg';
+import noveltyCake from '../../assets/novelty_cake.jpg';
+import cupCake from '../../assets/cup_cake.jpg';
+
+const cakeInfo = [
+  {
+    title: 'Novelty Cakes',
+    img: noveltyCake
+  },
+  {
+    title: 'Themed Sponge Cakes',
+    img: spongeCake
+  },
+  {
+    title: 'Cup Cakes',
+    img: cupCake
+  },
+]
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <section className="slider">
+      <MySlider renderControls={(next, previous) => [
+          <button className="left_btn" onClick={previous}>&lt;</button>,
+          <button className="right_btn" onClick={next}>&gt;</button>
+      ]}>
+          {cakeInfo.map((cake, i) => (
+            <div className="slider_img_wrapper">
+              <img className="slider_img" src={cakeInfo[i].img} />
+            </div>
+          ))}
+      </MySlider>
+    </section>
   )
 }
 
