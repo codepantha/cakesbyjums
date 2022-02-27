d1009384111a6b94febe0f5397948f85de981536
codepanther <pezechigozirim@gmail.com>
2022-02-27 14:27:05 +0100

    Create the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './footer.css';
+import chat from '../../assets/chat.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="chat">
+        <a 
+          href="https://api.whatsapp.com/send?phone=2347037523771&text=Lynotek...%20Giving%20your%20business%20digital%20wings"
+        >
+          <img src={chat} />
+        </a>
+      </div>
+      <p className="footer_text">Made with ❤️ by 
+        <a href="https://codepantherr.github.io/Portfolio/"> Codepantherr</a>
+      </p>
+    </footer>
   )
 }
 
